47aff1c3aace145af8d1c8b172d173d4679e4dac
Deep Mahajan <you@example.com>
2023-02-26 18:07:52 -0600

    Caravana backend setup

---
+++ Backend/carvana_back/src/main/java/com/Carvana/carvana_back/CarvanaBackApplication.java
@@ -0,0 +1,13 @@
+package com.Carvana.carvana_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarvanaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarvanaBackApplication.class, args);
+	}
+
+}
---
+++ Backend/carvana_back/src/test/java/com/Carvana/carvana_back/CarvanaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Carvana.carvana_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarvanaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
